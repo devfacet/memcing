913526dfd2aa052fa02eca063d5be001ae6c8767
cmfatih <cmuratfatih@gmail.com>
2014-04-25 03:53:58 -0400

    Add test for REST (done)

--- test/test-rest.js
+++ test/test-rest.js
@@ -11,7 +11,7 @@ var cache   = require('../app/cache'),
 ;
 
 // Init vars
-var appCache    = cache({isDebug: true, limitInKB: 131072, eviction: true}), // 128MB
+var appCache    = cache({isDebug: false, limitInKB: 16384, eviction: true}), // 16MB
     appREST     = rest({http: {isEnabled: true}}, appCache),
     appRESTUrl  = 'http://' + appREST.addrOf('http')
 ;
@@ -44,8 +44,11 @@ describe('appREST', function() {
 // Test for HTTP requests
 describe('request', function() {
 
+  // Init vars
+  var reqHeaders = {'Content-Type': 'application/x-www-form-urlencoded'};
+
   // GET
-  describe('get ' + appRESTUrl, function() {
+  describe('GET ' + appRESTUrl, function() {
     it('should respond HTTP status code 200', function(done) {
       request(appRESTUrl, function (err, res) {
         if(!err) {
@@ -59,12 +62,14 @@ describe('request', function() {
   });
 
   // PUT entry
-  describe('put ' + appRESTUrl + '/entries/hello', function() {
-    it('should set an entry', function(done) {
+  var urlHello = appRESTUrl + '/entries/hello';
+
+  describe('PUT ' + urlHello, function() {
+    it('should set `hello` entry', function(done) {
       request({
         method: 'PUT',
-        uri : appRESTUrl + '/entries/hello',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        uri : urlHello,
+        headers: reqHeaders,
         body: 'val=world'
       }, function (err, res, body) {
         if(!err) {
@@ -83,13 +88,15 @@ describe('request', function() {
   });
 
   // PUT entry with expiration
-  describe('put ' + appRESTUrl + '/entries/bye', function() {
-    it('should set an entry with expiration time', function(done) {
+  var urlBye = appRESTUrl + '/entries/bye';
+
+  describe('PUT ' + urlBye, function() {
+    it('should set `bye` entry with expiration time', function(done) {
       var ts = new Date().getTime();
       request({
         method: 'PUT',
-        uri : appRESTUrl + '/entries/bye',
-        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        uri : urlBye,
+        headers: reqHeaders,
         body: 'val=world&exp=10'
       }, function (err, res, body) {
         if(!err) {
@@ -107,4 +114,89 @@ describe('request', function() {
     });
   });
 
+  // POST entry
+  var urlCounter = appRESTUrl + '/entries/counter';
+
+  describe('POST ' + urlCounter, function() {
+    it('should add `counter` entry', function(done) {
+      request({
+        method: 'POST',
+        uri : urlCounter,
+        headers: reqHeaders,
+        body: 'val=1'
+      }, function (err, res, body) {
+        if(!err) {
+          var resData = JSON.parse(body);
+          expect(res.statusCode).to.equal(200);
+          expect(resData).to.have.property('key', 'counter');
+          expect(resData).to.have.property('val', 1);
+          expect(resData).to.have.property('ts').to.be.above(0);
+          expect(resData).to.have.property('expTS', 0);
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+  });
+
+  // POST entry
+  describe('POST ' + urlCounter, function() {
+    it('should fail and respond HTTP status code 409', function(done) {
+      request({
+        method: 'POST',
+        uri : urlCounter,
+        headers: reqHeaders,
+        body: 'val=1'
+      }, function (err, res, body) {
+        if(!err) {
+          var resData = JSON.parse(body);
+          expect(res.statusCode).to.equal(409);
+          expect(resData.entry).to.have.property('key', 'counter');
+          expect(resData.entry).to.have.property('val', 1);
+          expect(resData.entry).to.have.property('ts').to.be.above(0);
+          expect(resData.entry).to.have.property('expTS', 0);
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+  });
+
+  // DELETE entry
+  describe('DELETE ' + urlCounter, function() {
+    it('should delete `counter` entry', function(done) {
+      request({
+        method: 'DELETE',
+        uri : urlCounter
+      }, function (err, res, body) {
+        if(!err) {
+          expect(res.statusCode).to.equal(200);
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+  });
+
+  // DELETE entries
+  var urlEntries = appRESTUrl + '/entries';
+
+  describe('DELETE ' + urlEntries, function() {
+    it('should delete all entries', function(done) {
+      request({
+        method: 'DELETE',
+        uri : urlEntries
+      }, function (err, res) {
+        if(!err) {
+          expect(res.statusCode).to.equal(200);
+          done();
+        } else {
+          done(err);
+        }
+      });
+    });
+  });
 });
